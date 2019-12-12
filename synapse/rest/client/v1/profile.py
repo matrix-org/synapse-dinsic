48029d0eab8038a009b2a56d73220a86cd6bbeae
Andrew Morgan <andrew@amorgan.xyz>
2019-12-12 11:05:40 +0000

    Back out NoneType change

--- synapse/rest/client/v1/profile.py
+++ synapse/rest/client/v1/profile.py
@@ -18,7 +18,6 @@ import logging
 
 from twisted.internet import defer
 
-from synapse.api.errors import SynapseError
 from synapse.http.servlet import RestServlet, parse_json_object_from_request
 from synapse.rest.client.v2_alpha._base import client_patterns
 from synapse.types import UserID
@@ -134,10 +133,10 @@ class ProfileAvatarURLRestServlet(RestServlet):
         is_admin = yield self.auth.is_server_admin(requester.user)
 
         content = parse_json_object_from_request(request)
-        new_avatar_url = content.get("avatar_url")
-
-        if new_avatar_url is None:
-            raise SynapseError(400, "Missing required key: avatar_url")
+        try:
+            new_avatar_url = content["avatar_url"]
+        except Exception:
+            defer.returnValue((400, "Unable to parse name"))
 
         yield self.profile_handler.set_avatar_url(
             user, requester, new_avatar_url, is_admin
