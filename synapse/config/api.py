8e2f2654e0fc9ac5677da93df8193aff1c076126
Andrew Morgan <andrew@amorgan.xyz>
2021-02-09 10:53:59 +0000

    Implement knocking as defined by https://github.com/matrix-org/matrix-doc/pull/2403
    This is the base knocking stuff, taken from https://github.com/matrix-org/synapse/pull/6739
    and does not include any public room directory changes.
    
    While knocking hasn't merged yet on mainline due to waiting on getting Complement
    into Synapse's CI, the code has been well-tested. CI tests will be merged into
    DINUM's fork of Synapse to ensure things do not break.

--- synapse/config/api.py
+++ synapse/config/api.py
@@ -1,4 +1,5 @@
 # Copyright 2015, 2016 OpenMarket Ltd
+# Copyright 2020 The Matrix.org Foundation C.I.C.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +14,18 @@
 # limitations under the License.
 
 from synapse.api.constants import EventTypes
+from synapse.api.room_versions import KNOWN_ROOM_VERSIONS, RoomVersions
 
 from ._base import Config
+
+# The default types of room state to send to users to are invited to or knock on a room.
+DEFAULT_ROOM_STATE_TYPES = [
+    EventTypes.JoinRules,
+    EventTypes.CanonicalAlias,
+    EventTypes.RoomAvatar,
+    EventTypes.RoomEncryption,
+    EventTypes.Name,
+]
 
 
 class ApiConfig(Config):
@@ -22,21 +33,22 @@ class ApiConfig(Config):
 
     def read_config(self, config, **kwargs):
         self.room_invite_state_types = config.get(
-            "room_invite_state_types",
-            [
-                EventTypes.JoinRules,
-                EventTypes.CanonicalAlias,
-                EventTypes.RoomAvatar,
-                EventTypes.RoomEncryption,
-                EventTypes.Name,
-            ],
+            "room_invite_state_types", DEFAULT_ROOM_STATE_TYPES
         )
+        msc2403_enabled = config.get("msc2403_enabled", False)
+        if msc2403_enabled:
+            # Enable the MSC2403 unstable room version
+            KNOWN_ROOM_VERSIONS.update(
+                {RoomVersions.MSC2403_DEV.identifier: RoomVersions.MSC2403_DEV}
+            )
 
     def generate_config_section(cls, **kwargs):
         return """\
         ## API Configuration ##
 
-        # A list of event types that will be included in the room_invite_state
+        # A list of event types from a room that will be given to users when they
+        # are invited to a room. This allows clients to display information about the
+        # room that they've been invited to, without actually being in the room yet.
         #
         #room_invite_state_types:
         #  - "{JoinRules}"
@@ -44,6 +56,14 @@ class ApiConfig(Config):
         #  - "{RoomAvatar}"
         #  - "{RoomEncryption}"
         #  - "{Name}"
+
+        # Uncomment to enable experimental room knocking support as defined by
+        # MSC2403.
+        #
+        # Note that the APIs used by this feature are unstable and will break in the
+        # future.
+        #
+        #msc2403_enabled: true
         """.format(
             **vars(EventTypes)
         )
