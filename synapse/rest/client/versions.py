e825356ac91d0717509cf9f3c3a6dc59ea4e8838
GuillaumeV <88834548+guillaumevillemont@users.noreply.github.com>
2022-06-15 11:42:53 +0200

    enable cross-signing

--- synapse/rest/client/versions.py
+++ synapse/rest/client/versions.py
@@ -72,7 +72,7 @@ class VersionsRestServlet(RestServlet):
                     # MSC2326.
                     "org.matrix.label_based_filtering": True,
                     # Implements support for cross signing as described in MSC1756
-                    # "org.matrix.e2e_cross_signing": True,
+                    "org.matrix.e2e_cross_signing": True,
                     # Implements additional endpoints as described in MSC2432
                     "org.matrix.msc2432": True,
                     # Tchap does not currently assume this rule for r0.5.0
